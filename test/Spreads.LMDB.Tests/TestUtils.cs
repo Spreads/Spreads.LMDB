0f4093ab6adb2d788107e1017db12bca523e32c3
buybackoff <vbaybekov@gmail.com>
2018-09-03 00:10:37 +0300

    Add TestUtils

---
+++ test/Spreads.LMDB.Tests/TestUtils.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Spreads.LMDB.Tests
+{
+    public static class TestUtils
+    {
+        public static string BaseDataPath = "./Data/tmp/TestData/";
+
+        public static void ClearAll([CallerFilePath]string groupPath = null)
+        {
+            var group = Path.GetFileNameWithoutExtension(groupPath);
+            var path = Path.Combine(BaseDataPath, group);
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+            }
+        }
+
+        public static string GetPath(
+            [CallerMemberName]string testPath = null,
+            [CallerFilePath]string groupPath = null,
+            bool clear = true)
+        {
+            var group = Path.GetFileNameWithoutExtension(groupPath);
+            var path = Path.Combine(BaseDataPath, group, testPath);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            else if (clear)
+            {
+                var di = new DirectoryInfo(path);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+
+            return path;
+        }
+    }
+}
